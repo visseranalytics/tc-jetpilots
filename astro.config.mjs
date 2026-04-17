0ffacbd54bb24894cc7674f6d582cf94deef38bd
Visser Analytics <64865032+visseranalytics@users.noreply.github.com>
2026-04-17 15:57:50 -0500

    Changed domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tcjetpilots.com',
+  site: 'https://tcjetpilots.visseranalytics.com',
   output: 'static',
   integrations: [sitemap()],
   vite: {
