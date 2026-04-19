df0fc44c0c91187c6434659d34e2dc8f523bbf26
Visser Analytics <64865032+visseranalytics@users.noreply.github.com>
2026-04-19 15:58:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tcjetpilots.visseranalytics.com',
+  site: 'https://tcjetpilots.com',
   output: 'static',
   integrations: [sitemap()],
   vite: {
